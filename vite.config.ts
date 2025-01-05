dcc43e3a94ec458fbb8fd59498428b69420b00a7
Eng <tanengkeen@gmail.com>
2025-01-05 16:33:16 +0800

    mount backend server

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/companies': {
+        target: process.env.VITE_BACKEND_API_DOMAIN,
+        changeOrigin: true,
+        secure: false
+      },
+      '/employees': {
+        target: process.env.VITE_BACKEND_API_DOMAIN,
+        changeOrigin: true,
+        secure: false
+      },
+      '/documents': {
+        target: process.env.VITE_BACKEND_API_DOMAIN,
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 });
